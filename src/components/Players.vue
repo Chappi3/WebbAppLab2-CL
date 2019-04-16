1577918f40fd4d2ebd6be379468ffcc88fbb65d1
Tonny Frisk <tonny035@hotmail.com>
2019-04-16 10:02:03 +0200

    add players component

---
+++ src/components/Players.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main">
+    <h1>Players</h1>
+    <div class="names">
+      <label for="Name">Name:</label>
+      <input type="text" name="Name" required>
+      <button type="submit">Add Player</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Players"
+};
+</script>
+
+<style>
+.main {
+  margin: auto;
+  text-align: center;
+  width: 50%;
+}
+.names {
+  display: grid;
+}
+</style>
