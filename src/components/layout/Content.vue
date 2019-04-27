270d03842d925ec88704942699e26d588c73e753
Tonny Frisk <tonny035@hotmail.com>
2019-04-27 11:03:48 +0200

    update Content
    add GameBoard to Content & fix to only one player

--- src/components/layout/Content.vue
+++ src/components/layout/Content.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="content" id="app">
-    <AddPlayer v-on:add-player="addPlayer"/>
-    <Players :players="players"/>
+    <div v-if="players.length<1">
+      <Players :players="players"/>
+      <AddPlayer v-on:add-player="addPlayer" class="addPlayers"/>
+    </div>
+    <div v-if="players.length>0">
+      <GameBoard/>
+    </div>
   </div>
 </template>
+
+
 
 <script>
 import Players from "../Players.vue";
 import AddPlayer from "../AddPlayer.vue";
+import GameBoard from "../GameBoard.vue";
 
 export default {
   name: "Content",
   components: {
     Players,
-    AddPlayer
+    AddPlayer,
+    GameBoard
   },
   data() {
     return {
@@ -34,5 +43,8 @@ export default {
   width: 100%;
   min-height: calc(100vh - 150px);
   text-align: center;
+}
+.addPlayers {
+  margin-bottom: 20px;
 }
 </style>
